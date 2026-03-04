e6a238f423a0074259d216f53744157b3c3b3d10
kabeer AR <kabeeratwork7@gmail.com>
2026-03-04 15:09:27 +0530

    second large

---
+++ secondLarge.js
@@ -0,0 +1,18 @@
+
+function bubbleSort(arr){
+    let n = arr.length-1;
+    for(let i=0;i<n;i++){
+        for(let j=0;j<n-i;j++){
+            if(arr[j]>arr[j+1]){
+                let temp = arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+    return arr;
+}
+let array = [6,5,4,3,2,1];
+console.log("after sorting",bubbleSort(array));
+let result = [...array];
+console.log(result[result.length-2])
