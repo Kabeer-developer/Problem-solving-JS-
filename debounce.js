88b3b746df256f25a5cf5e688e857607a7ae98aa
kabeer AR <kabeeratwork7@gmail.com>
2026-03-04 15:10:50 +0530

    debounce/throttle

---
+++ debounce.js
@@ -0,0 +1,19 @@
+
+function debounce(fn,delay){
+    let timer;
+    return function(){
+        clearTimeout(timer);
+        timer = setTimeout(fn,delay);
+    }
+}
+
+function sayHello(){
+    console.log("hello");
+}
+
+const debounceFunc = debounce(sayHello,1000);
+debounceFunc();
+debounceFunc();
+debounceFunc();
+debounceFunc();
+console.log("called 4 times");
